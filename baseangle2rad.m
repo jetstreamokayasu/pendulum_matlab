e44ccee02e737cfdf890d6f85152bb3168479e65
jetstreamokayasu <tokotsuku2@outlook.jp>
2020-11-10 02:42:00 +0900

    改めてモータの過渡特性同定開始

---
+++ baseangle2rad.m
@@ -0,0 +1,16 @@
+function rad = baseangle2rad(base_angle) 
+
+    BASE = double(0x3FFFF);
+
+    if base_angle >= ( BASE / 2)
+        
+        rad = 2*pi*(double(base_angle) - BASE) / BASE;
+        
+    else
+        
+        rad = 2*pi*double(base_angle) / BASE;
+        
+    end
+
+end
+    
